8af3bc02d12feb359494df9eb0e244b2d493443f
khaled-bahr <khaled.bahr@scandiweb.com>
2025-06-21 08:25:06 +0300

    Cleaning files and indent all code

---
+++ Frontend/src/components/mini-cart-attribute-values/mini-cart-attribute-values.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+interface AttributeValue {
+  id?: string;
+  displayValue: string;
+  value: string;
+}
+
+interface MiniCartAttributeValuesProps {
+  attributeType: string;
+  attribute: {
+    values: AttributeValue[];
+  };
+  selectedAttributeValueId: string;
+  getKebakAttributeName: () => string;
+}
+
+const MiniCartAttributeValues: React.FC<MiniCartAttributeValuesProps> = ({
+  attributeType,
+  attribute,
+  selectedAttributeValueId,
+  getKebakAttributeName,
+}) => {
+  if (attributeType === 'text') {
+    return (
+      <div className="attribute-values-text d-flex">
+        {attribute.values.map((value: AttributeValue, index) => (
+          <div
+            key={value.id ? value.id : `${value.displayValue}-${index}`}
+            className={`attribute-text m-1 p-2 d-flex justify-content-center align-items-center ${
+              selectedAttributeValueId === value.id && 'selected'
+            }`}
+          >
+            <div
+              data-testid={`cart-item-attribute-${getKebakAttributeName()}-${
+                getKebakAttributeName() +
+                (selectedAttributeValueId === value.id ? '-selected' : '')
+              }`}
+            >
+              {value.value}
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  if (attributeType === 'swatch') {
+    return (
+      <div className="attribute-values-swatch d-flex">
+        {attribute.values.map((value: AttributeValue, index) => (
+          <div
+            key={value.id ? value.id : `${value.displayValue}-${index}`}
+            className="swatch-container"
+          >
+            <div
+              className={`attribute-swatch ${
+                selectedAttributeValueId === value.id && 'selected'
+              }`}
+              style={{ backgroundColor: value.value }}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default MiniCartAttributeValues;
--- Frontend/src/components/mini-cart-attributes-values/mini-cart-attributes-values.tsx
+++ Frontend/src/components/mini-cart-attributes-values/mini-cart-attributes-values.tsx
@@ -1,5 +1,5 @@
 import Attribute from '../../types/attribute';
-import AttributeValue from '../../types/attribute-value';
+import MiniCartAttributeValues from '../mini-cart-attribute-values/mini-cart-attribute-values';
 
 interface MiniCartAttributesValuesProps {
   attribute: Attribute;
@@ -16,47 +16,6 @@ const MiniCartAttributesValues: React.FC<MiniCartAttributesValuesProps> = ({
     return attribute.name.toLowerCase().replace(/\s+/g, '-');
   };
 
-  const getAttributeValues = (attributeType: string) => {
-    if (attributeType === 'text') {
-      return (
-        <>
-          <div className={`attribute-values-text d-flex`}>
-            {attribute.values.map((value: AttributeValue, index) => (
-              <div
-                key={value.id ? value.id : `${value.displayValue}-${index}`}
-                className={`attribute-text m-1 p-2 d-flex justify-content-center align-items-center  ${selectedAttributeValueId === value.id && 'selected'}`}
-              >
-                <div
-                  data-testid={`cart-item-attribute-${getKebakAttributeName()}-${getKebakAttributeName() + (selectedAttributeValueId === value.id ? '-selected' : '')}`}
-                >
-                  {value.value}
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
-      );
-    }
-
-    if (attributeType === 'swatch') {
-      return (
-        <div className="attribute-values-swatch d-flex">
-          {attribute.values.map((value: AttributeValue, index) => (
-            <div
-              key={value.id ? value.id : `${value.displayValue}-${index}`}
-              className="swatch-container"
-            >
-              <div
-                className={`attribute-swatch m-1 ${selectedAttributeValueId === value.id && 'selected'}`}
-                style={{ backgroundColor: value.value }}
-              />
-            </div>
-          ))}
-        </div>
-      );
-    }
-  };
-
   return (
     <>
       <div
@@ -70,7 +29,24 @@ const MiniCartAttributesValues: React.FC<MiniCartAttributesValuesProps> = ({
           {attribute.name} :{' '}
         </div>
         <div className="attribute-values">
-          {getAttributeValues(attribute.type)}
+          {
+            <MiniCartAttributeValues
+              attributeType={attribute.type}
+              attribute={{
+                ...attribute,
+                values: attribute.values.map(v => ({
+                  ...v,
+                  id: String(v.id),
+                })),
+              }}
+              selectedAttributeValueId={
+                selectedAttributeValueId !== undefined
+                  ? String(selectedAttributeValueId)
+                  : ''
+              }
+              getKebakAttributeName={getKebakAttributeName}
+            />
+          }
         </div>
       </div>
     </>
