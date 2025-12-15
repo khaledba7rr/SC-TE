cdaf3bea2908d5bddf1dfbbe16594c4d751b2a6e
khaled-bahr <khaled.bahr@scandiweb.com>
2025-12-15 06:42:43 +0200

    Correcting the Uri for the apollo to be for the live server instead of localhost

--- Frontend/src/data/apollo-client.ts
+++ Frontend/src/data/apollo-client.ts
@@ -2,8 +2,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  // uri: 'https://bahr-backend-scandi.duckdns.org/graphql', // live endpoint
-  uri: 'http://localhost:8000/graphql', // Local development endpoint
+  uri: 'https://bahr-backend-scandi.duckdns.org/graphql', // live endpoint
+  // uri: 'http://localhost:8000/graphql', // Local development endpoint
   cache: new InMemoryCache(),
 });
 
